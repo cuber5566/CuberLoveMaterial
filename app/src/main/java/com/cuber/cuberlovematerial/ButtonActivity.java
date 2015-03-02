94a8fc8337f990f18693eb2055dc2ed9a5414a04
Cuber <cuber5566@gmail.com>
2015-03-03 01:07:41 +0800

    fix

--- app/src/main/java/com/cuber/cuberlovematerial/ButtonActivity.java
+++ app/src/main/java/com/cuber/cuberlovematerial/ButtonActivity.java
@@ -3,14 +3,10 @@ package com.cuber.cuberlovematerial;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-import com.cuber.cuberlovematerial.views.FlatButton;
-
 /**
  * Created by cuber on 2015/2/26.
  */
 public class ButtonActivity extends ActionBarActivity {
-
-    FlatButton disabledFlatButton, hoverFlatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
